dcf81cdf88a5930aeb4fe3426e13302a78defb24
TheRealPauli <pj.rambow@gmail.com>
2017-11-14 12:16:40 +0100

    improved Gallery

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 var imageDir = "src/images/";
-var fileextention = ".png";
+var fileextention = ".JPG";
 
 $(document).ready(function() {
   $.ajax({
     url: imageDir,
     success: function(data) {
       $(data).find("a:contains(" + fileextention + ")").each(function() {
-        var filename = this.href.replace(window.location, "").replace("http://", "");
+        var filename = this.href.replace(window.location.host, "").replace("http://", "");
         $("#gallery").append("<img src='" + filename + "'>");
       });
     }
